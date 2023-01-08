e9ea9d5336a9eacc1be869ca1f6b7687f068ba7a
nairadethya2208 <88815815+nairadethya2208@users.noreply.github.com>
2023-01-08 23:25:01 +0530

    Color detection code is documented

--- color.py
+++ color.py
@@ -5,20 +5,28 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
+
+    #capturing the height and width of the frame.
     width = int(cap.get(3))
     height = int(cap.get(4))
 
+    #Converting the image from BGR to HSV.
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_blue = np.array([90, 50, 50])
     upper_blue = np.array([130, 255, 255])
 
+    #Creating a mask of color to be detected
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
+    #Creating the final image after selecting the colors of the required pixels
+    #using bitwise and operation. 
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
+    #Displaying the final result 
     cv2.imshow('frame', result)
     cv2.imshow('mask', mask)
 
+    #The frame will be closed when the key 'q' is pressed
     if cv2.waitKey(1) == ord('q'):
         break
 
